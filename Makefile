1dcad467671be2574d568e86644073f22fa1342f
naritta <narittan@gmail.com>
2022-12-18 14:16:13 +0900

    Use nlohmann::json

--- Makefile
+++ Makefile
@@ -4,8 +4,8 @@ clean:
 	rm -f *.o out
 
 test:
-	g++ -c -o main.o main.cpp
-	g++ -c -o json_reader.o json_reader.cpp
+	g++ -c -o main.o main.cpp -std=c++17
+	g++ -c -o json_reader.o json_reader.cpp -std=c++17
 	g++ -o out main.o json_reader.o
 
 run:
--- json_reader.cpp
+++ json_reader.cpp
@@ -1,6 +1,14 @@
 #include "json_reader.h"
 #include <cstdio>
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+using namespace std;
 
 void read() {
-  printf("\n");
+  std::ifstream f("test.json");
+  json data = json::parse(f);
+  std::cout << data["key1"] << "\n";
 }
