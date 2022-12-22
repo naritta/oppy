8292b028400ffde48a805c9808fec794e26e7c7e
naritta <narittan@gmail.com>
2022-12-22 23:10:10 +0900

    Add client function

--- server.cpp
+++ server.cpp
@@ -13,6 +13,11 @@ int main() {
 		std::cout << "Error socket:" << std::strerror(errno);
 		exit(1);
 	}
+  struct sockaddr_in addr;
+	memset(&addr, 0, sizeof(struct sockaddr_in));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(10000);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
   return 0;
 }
