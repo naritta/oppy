13e16dfe502edf0e14821dbd5403c8322f6515db
naritta <narittan@gmail.com>
2022-12-23 23:10:10 +0900

    Add mkdir function in c

--- server.cpp
+++ server.cpp
@@ -7,6 +7,24 @@
 
 using namespace std;
 
+static void _mkdir(const char *dir) {
+    char tmp[256];
+    char *p = NULL;
+    size_t len;
+
+    snprintf(tmp, sizeof(tmp),"%s",dir);
+    len = strlen(tmp);
+    if (tmp[len - 1] == '/')
+        tmp[len - 1] = 0;
+    for (p = tmp + 1; *p; p++)
+        if (*p == '/') {
+            *p = 0;
+            mkdir(tmp, S_IRWXU);
+            *p = '/';
+        }
+    mkdir(tmp, S_IRWXU);
+}
+
 int main() {
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sockfd < 0){
